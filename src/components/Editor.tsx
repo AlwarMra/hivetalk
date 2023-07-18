f380ed624ec2db9dff1eec6be554b0612a40bdd2
alwar <alwaromoreno@gmail.com>
2023-07-18 14:49:27 +0200

    create post editor

---
+++ src/components/Editor.tsx
@@ -0,0 +1,113 @@
+'use client'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+import TextareaAutosize from 'react-textarea-autosize'
+import { useForm } from 'react-hook-form'
+import { PostCreationRequest, postValidator } from '@/lib/validators/post'
+import { zodResolver } from '@hookform/resolvers/zod'
+import type EditorJS from '@editorjs/editorjs'
+import { uploadFiles } from '@/lib/uploadthing'
+
+interface EditorProps {
+  honeycombId: string
+}
+
+const Editor: FC<EditorProps> = ({ honeycombId }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(postValidator),
+    defaultValues: {
+      honeycombId,
+      title: '',
+      content: null,
+    },
+  })
+
+  const editorRef = useRef<EditorJS>()
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setIsMounted(true)
+    }
+  }, [])
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import('@editorjs/editorjs')).default
+    const Header = (await import('@editorjs/header')).default
+    const Embed = (await import('@editorjs/embed')).default
+    const Table = (await import('@editorjs/table')).default
+    const List = (await import('@editorjs/list')).default
+    const Code = (await import('@editorjs/code')).default
+    const LinkTool = (await import('@editorjs/link')).default
+    const InlineCode = (await import('@editorjs/inline-code')).default
+    const ImageTool = (await import('@editorjs/image')).default
+
+    if (!editorRef.current) {
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          editorRef.current = editor
+        },
+        placeholder: 'Type here to write your post...',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: { endpoint: '/api/link' },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], 'imageUploader')
+                  return { success: 1, file: { url: res.fileUrl } }
+                },
+              },
+            },
+          },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          embed: Embed,
+          table: Table,
+        },
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor()
+    }
+    setTimeout(() => {})
+
+    if (isMounted) {
+      init()
+
+      return () => {}
+    }
+  }, [isMounted, initializeEditor])
+
+  return (
+    <div className='w-full p-4 bg-amber-50 rounded-lg border border-amber-200'>
+      <form id='honeycomb-post-form' className='w-fit' onSubmit={() => {}}>
+        <div className='prose prose-stone dark:prose-invert'>
+          <TextareaAutosize
+            placeholder='Title'
+            className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none'
+          />
+
+          <div id='editor' className='m-h-[500px]' />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Editor
