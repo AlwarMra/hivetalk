ffa978fffce75295f3c4ae61eee2d1f99cf23be4
alwar <alwaromoreno@gmail.com>
2023-07-18 14:43:39 +0200

    create post under honeycomb page detail

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 import SubscribeLeaveToggle from '@/components/SubscribeLeaveToggle'
+import { buttonVariants } from '@/components/ui/Button'
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { format } from 'date-fns'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -92,6 +94,15 @@ const Layout = async ({
                   />
                 </div>
               ) : null}
+              <Link
+                href={`r/${slug}/submit`}
+                className={buttonVariants({
+                  variant: 'outline',
+                  className: 'w-full mb-6',
+                })}
+              >
+                Create post
+              </Link>
             </dl>
           </div>
         </div>
