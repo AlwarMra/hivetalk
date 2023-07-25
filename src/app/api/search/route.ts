95c39524dd0d3158ccb995c6e0642a69e62d5c9b
alwar <alwaromoreno@gmail.com>
2023-07-25 12:54:46 +0200

    remove unused try catch

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,23 +1,21 @@
 import { db } from '@/lib/db'
 
 export async function GET(req: Request) {
-  try {
-    const url = new URL(req.url)
-    const q = url.searchParams.get('q')
+  const url = new URL(req.url)
+  const q = url.searchParams.get('q')
 
-    if (!q) return new Response('Invalid query', { status: 400 })
+  if (!q) return new Response('Invalid query', { status: 400 })
 
-    const results = await db.honeycomb.findMany({
-      where: {
-        name: {
-          startsWith: q,
-        },
+  const results = await db.honeycomb.findMany({
+    where: {
+      name: {
+        startsWith: q,
       },
-      include: {
-        _count: true,
-      },
-      take: 5,
-    })
-    return new Response(JSON.stringify(results))
-  } catch (err) {}
+    },
+    include: {
+      _count: true,
+    },
+    take: 5,
+  })
+  return new Response(JSON.stringify(results))
 }
