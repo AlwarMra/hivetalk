db780cfbb6826f63a4f2c5b00aaa8994fa151da8
alwar <alwaromoreno@gmail.com>
2023-07-25 12:28:15 +0200

    user settings: update username

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,31 @@
+export const metadata = {
+  title: 'Settings',
+  description: 'Manage account settings',
+}
+
+import UsernameForm from '@/components/UsernameForm'
+import { authOptions, getAuthSession } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const page = async ({}) => {
+  const session = await getAuthSession()
+
+  if (!session?.user) {
+    redirect(authOptions.pages?.signIn || '/sign-in')
+  }
+  return (
+    <div className='max-w-4xl mx-auto py-12'>
+      <div className='grid items-start gap-8'>
+        <h1 className='font-bold text-3xl md:text-4xl'>Settings</h1>
+      </div>
+      <div className='grid gap-10 mt-4'>
+        <UsernameForm
+          user={{ id: session.user.id, username: session.user.username || ' ' }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default page
